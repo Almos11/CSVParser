96346fe2c0f25d43d17da88d929913236884370b
Almos11 <amosin2002@mail.ru>
2023-02-19 02:33:48 +0300

    Create project

---
+++ my_parser/cmake-build-debug/CMakeFiles/my_parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_parser.dir/main.cpp.o"
+  "CMakeFiles/my_parser.dir/main.cpp.o.d"
+  "my_parser"
+  "my_parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
